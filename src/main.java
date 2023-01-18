6e5bb0bbbe80943de0c48d94268a1c0e9ade753b
natthankrish <92136335+natthankrish@users.noreply.github.com>
2023-01-18 21:55:51 +0700

    Initialize Token

---
+++ src/main.java
@@ -0,0 +1,7 @@
+import struct.*;
+
+public class Main {    
+    public static void main (String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/struct/Token.java
@@ -0,0 +1,10 @@
+package struct;
+
+public class Token {
+    String[] allowedChar = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "J", "Q", "K"};
+    String[] operator = {"+", "-", "*", "/", "(", ")"};
+
+    public static void main (String[] args) {
+        System.out.println("Hello world");
+    }
+}
