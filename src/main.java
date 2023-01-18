858073731d4a84da3f26c9d13ef4cdb60ed06bed
natthankrish <92136335+natthankrish@users.noreply.github.com>
2023-01-18 22:52:17 +0700

    Initializing Deck

--- src/main.java
+++ src/main.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 import struct.*;
 
 public class Main {    
     public static void main (String[] args) {
-        System.out.println("Hello world");
+        String[] inputCards = new String[4];
+        Token Deck = new Token();
+        Scanner inputTray = new Scanner(System.in);
+
+        for (int i = 0; i < 4; i++) {
+            inputCards[i] = inputTray.next();
+        }
+
+        Deck.constructDeck(inputCards);
+
+        if (!Deck.isDeckValid(inputCards)) {
+            System.out.println("Simbol kartu tidak sesuai. Silahkan masukkan input yang benar!");
+        } else {
+            System.out.println("Halo");
+        }
+        
+        inputTray.close();
     }
 }
--- src/struct/Token.java
+++ src/struct/Token.java
@@ -1,10 +1,39 @@
 package struct;
 
 public class Token {
-    String[] allowedChar = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "J", "Q", "K"};
-    String[] operator = {"+", "-", "*", "/", "(", ")"};
+    final String[] allowedChar = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "J", "Q", "K"};
+    final String[] operator = {"+", "-", "*", "/", "(", ")"};
+    String[] cardDeck;
 
-    public static void main (String[] args) {
-        System.out.println("Hello world");
+    public void constructDeck (String[] inputCards) {
+        // Melakukan Inisialisasi Deck
+        this.cardDeck = new String[inputCards.length];
+        for (int i = 0; i < inputCards.length; i++) {
+            this.cardDeck[i] = inputCards[i];
+        }
     }
+
+    public boolean isDeckValid (String[] cardIn) {
+        // Melakukan check apakah input merupakan ranks dari kartu. 
+        boolean valid = true;
+        int i = 0;
+        while (i < cardIn.length && valid) {
+            int j = 0;
+            boolean found = false;
+            while (j < allowedChar.length && !found) {
+                System.out.println(cardIn[i] + " " + allowedChar[j]);
+                if (allowedChar[j].equals(cardIn[i])) {
+                    found = true;
+                }
+                j++;
+            }
+            valid = found;
+            i++;
+        }
+        return valid;
+    }
+    // public static void main (String[] args) {
+    //     Token a = new Token();
+    //     System.out.println(a.checkInput("1"));
+    // }
 }
